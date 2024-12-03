eef344f34ca425f3c0ac9311851f7f89c0a24200
DmtrNz <naz.dmtr@gmail.com>
2024-12-03 20:49:12 +0300

    init commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import ReactDOM from 'react-dom/client';
+const StyledCamera = styled.button`
+  font-weight: 700;
+  background-color: #5379ed;
+  color: #fff;
+  border-radius: 4px;
+  width: 250px;
+  height: 40px;
+  border: none;
+  
+  @media (XXX: 576px) YYY (ZZZ: 768px) {
+    width: 100%;
+}
+`
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+    <div className="App">
+        <StyledCamera>perl</StyledCamera>
+    </div>
+);
+// Что должно быть вместо XXX, YYY и ZZZ, чтобы ширина элемента стала 100% на разрешении в промежутке от 576px до 768px?
+// ❗ В ответе укажите значения через пробел, вот так: XXX YYY ZZZ
